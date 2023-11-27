015c3f899b0a88f1251550eff5aeccd26a4c90aa
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-11-27 15:01:55 -0300

    feat: requisito 11

---
+++ src/11_challenge.sql
@@ -0,0 +1,8 @@
+SELECT
+    albums.title AS 'Album',
+    albums.release_year AS 'Ano de lançamento',
+    artists.name AS 'Artista'
+FROM albums
+    JOIN artists ON albums.artist_id = artists.id
+WHERE albums.title LIKE '%you%'
+ORDER BY albums.release_year;
